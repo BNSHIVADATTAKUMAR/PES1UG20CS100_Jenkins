59b9ad4ace5e7940595e1755b319ad5d8f07febd
B N SHIVADATTA KUMAR <84439168+BNSHIVADATTAKUMAR@users.noreply.github.com>
2023-02-17 00:05:01 +0530

    Update working.cpp

--- working.cpp
+++ working.cpp
@@ -4,7 +4,7 @@ pipeline {
     stage('Build') {
       steps {
         sh 'g++ working.cpp'
-        build job: 'PES1UG20CS'
+        build job: 'PES1UG20CS100'
         echo 'Built Successfully '
       }
     }
